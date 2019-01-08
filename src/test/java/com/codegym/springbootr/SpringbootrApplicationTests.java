61fa4b561afcd42f0d35d2e9eacbd1553feacd7e
VuKieuAnh <vukieuanh.hnue@gmail.com>
2019-01-08 14:40:32 +0700

    init

---
+++ src/test/java/com/codegym/springbootr/SpringbootrApplicationTests.java
@@ -0,0 +1,17 @@
+package com.codegym.springbootr;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringbootrApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
